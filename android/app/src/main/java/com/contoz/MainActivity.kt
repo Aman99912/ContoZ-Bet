235d047851afc4a2e7412670af90ab18e8b97192
AmanDeep <amansaroha110@gmail.com>
2026-01-31 16:39:28 +0530

    sgdhfj

--- android/app/src/main/java/com/contoz/MainActivity.kt
+++ android/app/src/main/java/com/contoz/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.amandeep_saroha.ContoZ
+package com.contoz
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/contoz/MainApplication.kt
+++ android/app/src/main/java/com/contoz/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.amandeep_saroha.ContoZ
+package com.contoz
 
 import android.app.Application
 import android.content.res.Configuration
